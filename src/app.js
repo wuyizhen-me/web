8af5c91137eb8f82d25245a8f24ce8e36d1b3cac
wuyizhen-me <142096825+wuyizhen-me@users.noreply.github.com>
2025-03-29 00:05:29 +0800

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Threads from './Threads';
+
+<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <Threads
+    amplitude={1}
+    distance={0}
+    enableMouseInteraction={true}
+  />
+</div>
